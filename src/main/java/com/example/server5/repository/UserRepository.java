95d105cddcf87970da66d952ca75b9d9f2fec66a
valhub08 <jhstl1203@gmail.com>
2024-09-12 23:19:31 +0900

    firstCommit

---
+++ src/main/java/com/example/server5/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.server5.repository;
+
+
+import com.example.server5.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
+}
