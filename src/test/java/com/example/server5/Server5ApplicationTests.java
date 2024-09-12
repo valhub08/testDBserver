eb452532da6274581f578ee9f48d8bea66399f4f
valhub08 <jhstl1203@gmail.com>
2024-09-12 23:20:24 +0900

    Application

---
+++ src/test/java/com/example/server5/Server5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.server5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Server5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
